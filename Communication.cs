35306a02eadc70c356db73e71fcc300b244747c0
Luciana da Costa Marques <lucianadacostamarques@gmail.com>
2018-09-29 22:00:42 -0300

    Add files via upload

--- Communication.cs
+++ Communication.cs
@@ -6,20 +6,30 @@ using UnityEngine.Networking;
 
 public class Communication : MonoBehaviour {
 
+    //This class is responsible for communication between Unity and Server
+
+    //Player's actions management
+    
+    //Class with directions, actions and coloured shots
     [Serializable]
     public class Actions
     {
-        public bool port;
-        public bool starboard;
+        public float aimAngle;
         public bool fixGenerator;
         public bool loadCyan;
         public bool loadGreen;
         public bool loadPurple;
         public bool loadWhite;
+        public bool port;
+        public int[] roomState;
+        public bool starboard;
     }
 
     public static Actions playerActions = new Actions();
 
+    public int update = 0; 
+
+    //Method to get the class's atribute values
     public IEnumerator GetPlayerActions()
     {
 
@@ -31,8 +41,29 @@ public class Communication : MonoBehaviour {
 
         JsonUtility.FromJsonOverwrite(json, playerActions);
 
+        Debug.Log("port");
+        Debug.Log(playerActions.port);
+        Debug.Log("starboard");
+        Debug.Log(playerActions.starboard);
+        Debug.Log("fixGenerator");
+        Debug.Log(playerActions.fixGenerator);
+        Debug.Log("loadCyan");
+        Debug.Log(playerActions.loadCyan);
+        Debug.Log("loadGreen");
+        Debug.Log(playerActions.loadGreen);
+        Debug.Log("loadPurple");
+        Debug.Log(playerActions.loadPurple);
+        Debug.Log("loadWhite");
+        Debug.Log(playerActions.loadWhite);
+        Debug.Log("aim angle");
+        Debug.Log(playerActions.aimAngle.ToString());
+        //Debug.Log("room state");
+        //Debug.Log(playerActions.roomState.ToString());
     }
 
+    //Shots Management
+
+    //Class with atributes of taken shots and fired shots
     [Serializable]
     public class Shots
     {
@@ -42,6 +73,7 @@ public class Communication : MonoBehaviour {
 
     public static Shots playerShots = new Shots();
 
+    //Method to warn Server of fired and taken shots
     IEnumerator UpdateShotsTaken()
     {
         WWWForm form = new WWWForm();
@@ -61,7 +93,11 @@ public class Communication : MonoBehaviour {
         }
         else
         {
-            Debug.Log("Updated Player Energy");
+            Debug.Log("Updated Player Shots");
+            Debug.Log("shots taken");
+            Debug.Log(playerShots.taken);
+            Debug.Log("shots fired");
+            Debug.Log(playerShots.fired);
         }
 
     }
@@ -70,9 +106,23 @@ public class Communication : MonoBehaviour {
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per fixed frame time
+    // Gets player's actions and Update Server on shots info
+    private void FixedUpdate()
+    {
+        if (update == 3)
+        {
+            StartCoroutine(GetPlayerActions());
+            update = 0;
+        }
+        else
+        {
+            update++;
+        }
+        if (shot)
+        {
+            StartCoroutine(UpdateShotsTaken());
+        }
+    }
 }
